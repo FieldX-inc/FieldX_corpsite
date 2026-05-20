ae28cbecd139431b78b0603b23a5c62c27e6d757
Yoshihiko Sato <yshk@YoshihikonoMacBook-Air.local>
2026-05-20 17:14:58 +0900

    Show published news on home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { siteContent } from "@/components/site/content";
 import { HomeTemplate } from "@/components/templates";
 import { getColumnPosts } from "@/lib/content/repository";
+import { publishedNewsPosts } from "@/lib/news";
 
 export const revalidate = 300;
 
@@ -8,5 +9,5 @@ export default async function HomePage() {
   const allColumnPosts = await getColumnPosts();
   const columnPosts = allColumnPosts.slice(0, 3);
 
-  return <HomeTemplate content={siteContent} columnPosts={columnPosts} newsPosts={[]} />;
+  return <HomeTemplate content={siteContent} columnPosts={columnPosts} newsPosts={publishedNewsPosts} />;
 }
